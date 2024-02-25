ab79637e8b4afe03e7f916e3a50d14f88700a183
Chaitya Shah <102247651+Chaitya54@users.noreply.github.com>
2024-02-25 21:54:53 +0530

    Time: 955 ms (88.44%), Space: 0B (100.00%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -3,6 +3,6 @@ with RankedDates as(
     select *, row_number() over(partition by a.customer_id order by a.order_date) as rn
     from delivery as a
 )
-select round((count(case when b.order_date = b.customer_pref_delivery_date then 1 end) / count(b.customer_id)) * 100 ,2) as immediate_percentage
+select coalesce(round((count(case when b.order_date = b.customer_pref_delivery_date then 1 end) / count(b.customer_id)) * 100 ,2), 0) as immediate_percentage
 from RankedDates as b
 where b.rn = 1
