7028507ee59a0189bd26a278c1e4a32e005bfb6c
Chaitya Shah <102247651+Chaitya54@users.noreply.github.com>
2024-02-25 21:54:14 +0530

    Time: 1361 ms (27.31%), Space: 0B (100.00%) - LeetHub

---
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+with RankedDates as(
+    select *, row_number() over(partition by a.customer_id order by a.order_date) as rn
+    from delivery as a
+)
+select round((count(case when b.order_date = b.customer_pref_delivery_date then 1 end) / count(b.customer_id)) * 100 ,2) as immediate_percentage
+from RankedDates as b
+where b.rn = 1
