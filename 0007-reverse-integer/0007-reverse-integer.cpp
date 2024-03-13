b24bfe3cbeffa8786602462643bdabc5449c6334
Chaitya Shah <102247651+Chaitya54@users.noreply.github.com>
2024-03-13 16:40:45 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (46.39%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    long reverse(long n) {
+        if(n<INT_MIN || n>INT_MAX){
+            return 0;
+        }
+        long ans=0;
+            while(n!=0){
+                int d= n%10;
+                if(ans>INT_MAX/10 || ans < INT_MIN/10){
+                    return 0;
+                }
+                ans = ans*10 + d;
+                n/=10;
+            }
+            return ans;    
+        // else{
+        //     while(n!=0){
+        //         int d= n%10;
+        //         ans = ans*10 + d;
+        //         n/=10;
+        //     }
+        //     return ans;
+        // }
+    }
+};
