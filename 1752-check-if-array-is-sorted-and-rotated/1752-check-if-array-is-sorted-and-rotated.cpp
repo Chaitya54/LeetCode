49fd4b2eb7b495b6cf59af44dd67ef73dcf1f697
Chaitya Shah <102247651+Chaitya54@users.noreply.github.com>
2024-04-10 23:49:16 +0530

    Time: 5 ms (20.92%), Space: 10.4 MB (5.09%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,34 +1,49 @@
+// class Solution {
+// public:
+//     bool check(vector<int>& nums) {
+//         int n = nums.size();
+//         int min = nums[0];
+//         int start = -1;
+//         for(int i=1; i<n; i++){
+//             if(nums[i]>=nums[i-1]){
+                
+//             }
+//             else{
+//                 start=i;
+//                 break;
+//             }
+//         }
+//         if(start == -1){
+//             return true;
+//         }
+//         else if(start == n-1 && nums[n-1]>min){
+//             return false;
+//         }
+//         else{
+//             for(int i = start+1 ; i<n; i++){
+//                 if(nums[i]>=nums[i-1] && nums[i]<=min){
+                
+//                 }
+//                 else{
+//                     return false;
+//                 }
+//             }
+//             return true;
+//         }
+//     }
+// };
+
+
+
 class Solution {
 public:
-    bool check(vector<int>& nums) {
-        int n = nums.size();
-        int min = nums[0];
-        int start = -1;
-        for(int i=1; i<n; i++){
-            if(nums[i]>=nums[i-1]){
-                
-            }
-            else{
-                start=i;
-                break;
+    bool check(vector<int> nums) {
+        int cnt = 0;
+        for (int i = 0, n = nums.size(); i < n; ++i) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                ++cnt;
             }
         }
-        if(start == -1){
-            return true;
-        }
-        else if(start == n-1 && nums[n-1]>min){
-            return false;
-        }
-        else{
-            for(int i = start+1 ; i<n; i++){
-                if(nums[i]>=nums[i-1] && nums[i]<=min){
-                
-                }
-                else{
-                    return false;
-                }
-            }
-            return true;
-        }
+        return cnt <= 1;
     }
 };
