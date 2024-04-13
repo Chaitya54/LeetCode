7a12a8d9515a4a29048af8e5923898b534b16119
Chaitya Shah <102247651+Chaitya54@users.noreply.github.com>
2024-04-13 22:10:08 +0530

    Time: 15 ms (50.47%), Space: 20.3 MB (60.08%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -16,11 +16,22 @@ public:
 //         }
 //         return 0;
         
-        int n=nums.size();
-        int sum = n*(n+1)/2;
+        // int n=nums.size();
+        // int sum = n*(n+1)/2;
+        // for(int i=0; i<n; i++){
+        //     sum-=nums[i];
+        // }
+        // return sum;
+        
+        int n = nums.size();
+        int XOR1=0;
+        int XOR2=0;
         for(int i=0; i<n; i++){
-            sum-=nums[i];
+            XOR1 = XOR1 ^ (i+1);
+            XOR2 = XOR2 ^ nums[i];
         }
-        return sum;
+        // XOR1 = XOR1 ^ (n+1);
+        return XOR1 ^ XOR2;
+        
     }
 };
