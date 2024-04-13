ef33519b43649681f0bafc63e26163eb2534e9c6
Chaitya Shah <102247651+Chaitya54@users.noreply.github.com>
2024-04-14 02:10:37 +0530

    Time: 8 ms (89.58%), Space: 20.4 MB (33.73%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n=nums.size();
-        vector <int> hash(n+1,0);
-        for(int i=0;i<n;i++){
-            hash[nums[i]]++;
-        }
-        for(int i=0;i<n+1;i++){
-            if(hash[i]==0){
-                return i;
-            }
-        }
-        return 0;
+        // int n=nums.size();
+        // vector <int> hash(n+1,0);
+        // for(int i=0;i<n;i++){
+        //     hash[nums[i]]++;
+        // }
+        // for(int i=0;i<n+1;i++){
+        //     if(hash[i]==0){
+        //         return i;
+        //     }
+        // }
+        // return 0;
         
         
         
@@ -24,14 +24,14 @@ public:
         
         
         
-        // int n = nums.size();
-        // int XOR1=0;
-        // int XOR2=0;
-        // for(int i=0; i<n; i++){
-        //     XOR1 = XOR1 ^ (i+1);
-        //     XOR2 = XOR2 ^ nums[i];
-        // }
-        // return XOR1 ^ XOR2;
+        int n = nums.size();
+        int XOR1=0;
+        int XOR2=0;
+        for(int i=0; i<n; i++){
+            XOR1 = XOR1 ^ (i+1);
+            XOR2 = XOR2 ^ nums[i];
+        }
+        return XOR1 ^ XOR2;
         
     }
 };
