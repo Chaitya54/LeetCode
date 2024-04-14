898dc76161bb28f2c539ef5d2e469aa40c9e9894
Chaitya Shah <102247651+Chaitya54@users.noreply.github.com>
2024-04-14 18:15:00 +0530

    Time: 1078 ms (35.93%), Space: 0B (100.00%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -8,8 +8,8 @@ select product_id, new_price as price
 from Products
 where(product_id, change_date) in
 (
-    select p.product_id, max(p.change_date)
-    from Products p
-    where p.change_date<='2019-08-16'
-    group by p.product_id
+    select product_id, max(change_date)
+    from Products
+    where change_date<='2019-08-16'
+    group by product_id
 )
